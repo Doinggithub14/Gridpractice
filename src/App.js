3558192edc2fd27e9a1ba6b8b986d8b2a1b159b9
Doinggithub14 <rewavadnere17@gmail.com>
2024-07-02 22:20:28 +0530

    im back modified againg

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ return(<div className="blank">
   <Grid container spacing={3}>
     
     <Grid alignItems={"center"} item xs={12} sm={4} md={3} sx={{height:"250px", backgroundColor:"powderblue"}}>
-        <MediaCard title="I'm Here!" pic="https://i.pinimg.com/564x/d8/60/d5/d860d584fc94c31814ca74d5f52d0526.jpg" about="yes!"/>
+        <MediaCard title="Divya backk" pic="https://i.pinimg.com/564x/d8/60/d5/d860d584fc94c31814ca74d5f52d0526.jpg" about="yes!"/>
          
     </Grid>
 
     <Grid item xs={12} sm={4} md={3} sx={{height:"250px", backgroundColor:"yellow"}}>
-      <MediaCard title="asdvdv" pic="https://i.pinimg.com/564x/d8/60/d5/d860d584fc94c31814ca74d5f52d0526.jpg" about="yes!"/>
+      <MediaCard title="asdvd" pic="https://i.pinimg.com/564x/d8/60/d5/d860d584fc94c31814ca74d5f52d0526.jpg" about="yes!"/>
        
     </Grid>
 
